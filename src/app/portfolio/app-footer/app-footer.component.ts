073febc035996f5e71c8e2ca37903b915bd1b5c3
Saghar Noursobhi <saghar.noursobhi@hyperisland.se>
2024-07-09 14:04:15 +0330

    create fancy animation for about-me component via: template reference variables, HostListener to bind the function to the scroll event and ngAfterViewInitHook to show the result only after completion of component view initialization. creating mailto, tel schemes defined in HTML structure.

--- src/app/portfolio/app-footer/app-footer.component.ts
+++ src/app/portfolio/app-footer/app-footer.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-footer.component.scss']
 })
 export class AppFooterComponent {
+  phoneNumber: string | undefined;
+  gmail: string | undefined;
+  subject: any | undefined;
+  body: any | undefined;
+  constructor() {
+    this.phoneNumber = "+46737390072";
+    this.gmail = "sagharnoursobhi@gmail.com";
+    this.body = "Body Text";
+    this.subject = "Subject";
+  }
 
+  protected readonly encodeURIComponent = encodeURIComponent;
 }
